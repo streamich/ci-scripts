4c9089fbd1d8a4c11a4d0d4961d3221a911633a3
streamich <streamich@gmail.com>
2018-04-28 20:01:00 +0100

    fix: PROJECT_NAME and improve Slack script

--- ci.config.js
+++ ci.config.js
@@ -5,11 +5,6 @@ module.exports = {
     slack: {
         params: {
             webhook: 'https://hooks.slack.com/services/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',
-            text: ({PROJECT_NAME, BUILD_BRANCH, AWS_CLOUD_FRONT, UPLOAD_PATH}) => {
-                const url = `https://${AWS_CLOUD_FRONT}${UPLOAD_PATH}`;
-
-                return `${'`' + PROJECT_NAME + '`'} deployed ${'`' + BUILD_BRANCH + '`'} branch: ${url}`;
-            },
         },
     }
 };
