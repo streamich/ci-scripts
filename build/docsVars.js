a691c697b1181ec2151b19098552e1ed693d7c60
streamich <streamich@gmail.com>
2018-04-28 21:37:27 +0100

    style: add linter

--- build/docsVars.js
+++ build/docsVars.js
@@ -5,14 +5,17 @@ const glob = require('glob');
 const REG_BLOCK = /(^|\n)(\s*\/\/\/([^\n]*))+/g;
 const REG_LINE = /\s*\/\/\/\s?([^\n]*)/;
 
-const buildVarsReadme = () => {
+const docsVars = () => {
     let files;
 
     try {
         files = glob.sync('lib/vars/*.js');
     } catch (error) {
+        // eslint-disable-next-line no-console
         console.log(error);
+        // eslint-disable-next-line no-process-exit
         process.exit(1);
+
         return;
     }
 
@@ -21,13 +24,16 @@ const buildVarsReadme = () => {
     let text = '';
 
     for (const file of files) {
-        const filename = path.join(cwd, file)
+        const filename = path.join(cwd, file);
         const content = fs.readFileSync(filename, 'utf8');
         const blocks = content.match(REG_BLOCK);
         const name = path.basename(file).slice(0, -3);
         let fileText = '';
 
-        if (!blocks) continue;
+        if (!blocks) {
+            // eslint-disable-next-line no-continue
+            continue;
+        }
 
         list += '- [`' + name + '`](#' + name.toLowerCase() + '-variable)\n';
 
@@ -51,7 +57,8 @@ const buildVarsReadme = () => {
         text += `### \`${name}\` Variable\n\n` + fileText + '\n\n\n';
     }
 
+    // eslint-disable-next-line consistent-return
     return list + '\n\n' + text;
 };
 
-module.exports = buildVarsReadme;
+module.exports = docsVars;
