4c9ea226515512640af5ac0912cbe4596ca4de66
streamich <streamich@gmail.com>
2018-04-29 00:44:30 +0100

    feat: remove tokens and add Jest

--- ci.config.js
+++ ci.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    UPLOAD_PATH: 'XXXX',
-    AWS_CLOUD_FRONT: 'XXXX',
-
     slack: {
         params: {
-            webhook: 'https://hooks.slack.com/services/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',
+            webhook: '',
         },
     },
     'github-post': {
         params: {
-            token: '16fa3164dcd06eef0cb952a422cb260be8957fe'
+            token: ''
         }
     }
 };
