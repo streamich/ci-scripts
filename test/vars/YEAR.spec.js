9a757f960bd5446619f1f91cc71a92aa139337b0
streamich <streamich@gmail.com>
2018-04-29 01:11:31 +0100

    refactor: separate vars

--- test/vars/YEAR.spec.js
+++ test/vars/YEAR.spec.js
@@ -1,5 +1,7 @@
+const vars = require('../../lib/vars');
+
 describe('YEAR', () => {
     test('returns correct year', () => {
-
+        expect(vars.YEAR).toBe(String((new Date()).getFullYear()));
     });
 });
