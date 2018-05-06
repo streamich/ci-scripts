c400fe07f21017e8e70ae647b456ee3cb394fa0c
streamich <streamich@gmail.com>
2018-05-06 22:18:31 +0100

    refactor: separate ci object creation

---
+++ test/scripts/slack.spec.js
@@ -0,0 +1,42 @@
+const slack = require('../../lib/scripts/slack');
+const log = require('../../lib/effects/log');
+const request = require('../../lib/effects/request');
+
+jest.mock('../../lib/effects/log');
+jest.mock('../../lib/effects/request');
+
+const webhook = 'https://example.com/hook';
+
+describe('slack script', () => {
+    beforeEach(() => {
+        log.mockClear();
+        request.mockClear();
+    });
+
+    test('exists', () => {
+        expect(typeof slack).toBe('function');
+    });
+
+    test('throws if webhook not provided', async () => {
+        const ci = {
+            params: {},
+        };
+
+        try {
+            slack(ci, ci.params);
+            throw new Error('Did not throw on no WebHook.');
+        } catch (error) {
+            expect(Boolean(error.message.match(/webhook/i))).toBe(true);
+        }
+    });
+
+    test('posts message to slack and logs to console', async () => {
+        const ci = {
+            params: {
+                webhook
+            },
+        };
+
+        await slack(ci, ci.params);
+    });
+});
