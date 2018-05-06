eb356b841a6226b6e55b2ac5c4ba61cf51f99556
streamich <streamich@gmail.com>
2018-05-07 00:00:49 +0100

    test: add slack command integration test

--- test/scripts/slack.spec.js
+++ test/scripts/slack.spec.js
@@ -2,6 +2,7 @@ const slack = require('../../lib/cmd/slack');
 const log = require('../../lib/effects/log');
 const request = require('../../lib/effects/request');
 const createCi = require('../../lib/createCi');
+const exec = require('../..').exec;
 
 jest.mock('../../lib/effects/log');
 jest.mock('../../lib/effects/request');
@@ -48,25 +49,20 @@ describe('slack script', () => {
         expect(config.uri).toBe(webhook);
         expect(config.body.text.includes(ci.BUILD_VERSION)).toBe(true);
     });
-/*
-    test('posts message to slack and logs to console', async () => {
-        const ci = await createCi(['slack'], {webhook});
 
-        await slack(ci, ci.params);
+    test('integration - posts message to Slack', async () => {
+        await exec(['slack'], {webhook});
 
         expect(log).toHaveBeenCalledTimes(1);
 
         const postMessage = log.mock.calls[0][2];
 
         expect(Boolean(postMessage.match(/posted/i))).toBe(true);
-        expect(postMessage.includes(ci.BUILD_VERSION)).toBe(true);
 
         expect(request).toHaveBeenCalledTimes(1);
 
         const config = request.mock.calls[0][2];
 
         expect(config.uri).toBe(webhook);
-        expect(config.body.text.includes(ci.BUILD_VERSION)).toBe(true);
     });
-    */
 });
