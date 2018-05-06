f2fcfe05911a243cceb89b48fdc4ddabb6d593e7
streamich <streamich@gmail.com>
2018-05-06 23:41:30 +0100

    test: add echo command tests

--- test/scripts/echo.spec.js
+++ test/scripts/echo.spec.js
@@ -1,3 +1,4 @@
+const {execSync} = require('child_process');
 const echo = require('../../lib/scripts/echo');
 const log = require('../../lib/effects/log');
 
@@ -23,5 +24,23 @@ describe('echo script', () => {
         echo(ci, params);
 
         expect(log).toHaveBeenCalledWith(ci, params, 'foobar');
+    });
+
+    test('static message', () => {
+        expect(execSync('./bin/ci.js echo --message="foobar"').toString().trim()).toBe('foobar');
+    });
+
+    test('static message with --eval or -e', () => {
+        expect(execSync('./bin/ci.js echo --message="foobar" --eval').toString().trim()).toBe('foobar');
+        expect(execSync('./bin/ci.js echo --message="foobar" -e').toString().trim()).toBe('foobar');
+    });
+
+    test('evals variables with --eval and -e flags', () => {
+        const year = (new Date()).toJSON().substr(0, 4);
+
+        /* eslint-disable no-template-curly-in-string */
+        expect(execSync('./bin/ci.js echo --message="\\${YEAR}" --eval').toString().trim()).toBe(year);
+        expect(execSync('./bin/ci.js echo --message="\\${YEAR}" --e').toString().trim()).toBe(year);
+        /* eslint-enable no-template-curly-in-string */
     });
 });
